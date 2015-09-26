b9cd79cd60cc5560df0c9fe70a043ec6eaa4d342
Umut Sirin <umutsirin1@gmail.com>
2015-09-26 01:48:19 -0700

    Add brew recipes

---
+++ install/brew.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install Homebrew if it's not already installed
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Installing homebrew packages..."
+
+brew install ag
+brew install ack
+brew install tree
+brew install wget
+brew install z
+brew install cmake
+brew install tig
+
+brew install git
+brew install hub
+brew install macvim --override-system-vim
+brew install reattach-to-user-namespace
+brew install tmux
+brew install zsh
+
+brew install node
+brew install golang
+brew install python
+
+brew install boot2docker
+brew install mongodb
+brew install nginx
+brew install postgresql
+
+exit 0
