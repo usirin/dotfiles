2f4a3197e71b130433b49bcf289bf1a1cb9006dd
Umut Sirin <umutsirin1@gmail.com>
2016-06-18 00:30:03 -0700

    add load-nvm zsh hook

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,4 +6,12 @@ portlist() { sudo lsof -i :"$@" }
 
 kdbe() { ./configure && ./go/build.sh && supervisorctl reload }
 
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+
 
