17780086f2a551fb8826ee4fcc0f94d99a6fcfe8
Umut Sirin <umutsirin1@gmail.com>
2017-05-20 13:36:03 +0300

    update zsh fns
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,6 +6,48 @@ portlist() { sudo lsof -i :"$@" }
 
 kdbe() { ./configure && ./go/build.sh && supervisorctl reload }
 
+kd-conf() {
+  ./configure --host=koding-`whoami`.oud.cc:4480 --hostname=koding-`whoami`.oud.cc --publicPort 8090 --config dev
+}
+
+oud() { /tmp/ngrok -config=/tmp/.ngrok.conf -subdomain="$@".koding-`whoami` 127.0.0.1:8090 }
+
+dps()  {
+  docker ps $@ | awk '
+  NR==1{
+    FIRSTLINEWIDTH=length($0)
+    IDPOS=index($0,"CONTAINER ID");
+    IMAGEPOS=index($0,"IMAGE");
+    COMMANDPOS=index($0,"COMMAND");
+    CREATEDPOS=index($0,"CREATED");
+    STATUSPOS=index($0,"STATUS");
+    PORTSPOS=index($0,"PORTS");
+    NAMESPOS=index($0,"NAMES");
+    UPDATECOL();
+  }
+  function UPDATECOL () {
+    ID=substr($0,IDPOS,IMAGEPOS-IDPOS-1);
+    IMAGE=substr($0,IMAGEPOS,COMMANDPOS-IMAGEPOS-1);
+    COMMAND=substr($0,COMMANDPOS,CREATEDPOS-COMMANDPOS-1);
+    CREATED=substr($0,CREATEDPOS,STATUSPOS-CREATEDPOS-1);
+    STATUS=substr($0,STATUSPOS,PORTSPOS-STATUSPOS-1);
+    PORTS=substr($0,PORTSPOS,NAMESPOS-PORTSPOS-1);
+    NAMES=substr($0, NAMESPOS);
+  }
+  function PRINT () {
+    print ID NAMES IMAGE STATUS CREATED COMMAND PORTS;
+  }
+  NR==2{
+    NAMES=sprintf("%s%*s",NAMES,length($0)-FIRSTLINEWIDTH,"");
+    PRINT();
+  }
+  NR>1{
+    UPDATECOL();
+    PRINT();
+  }' | less -FSX;
+}
+dpsa() { dps -a $@; }
+
 autoload -U add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
@@ -13,5 +55,7 @@ load-nvmrc() {
   fi
 }
 add-zsh-hook chpwd load-nvmrc
+
+b2d() { eval "$(boot2docker shellinit)" }
 
 
